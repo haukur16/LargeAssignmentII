01bd32d2a026b9e3f065e49673530ea6322afaf2
Haukur Guðmundsson <haukur16@ru.is>
2019-02-11 19:51:45 +0000

    text

--- drawio.js
+++ drawio.js
@@ -5,13 +5,16 @@ window.drawio = {
     canvas: document.getElementById('my-canvas'),
     ctx: document.getElementById('my-canvas').getContext('2d'),
     colorPick: document.querySelector('.color-picker'),
+    widthPick: document.querySelector('.width-picker'),
+    textBox: document.getElementById('idTextBox'),
     selectedElement: null,
 
     availableShapes: {
         RECTANGLE: 'rectangle',
         CIRCLE: 'circle',
         LINE: 'line',
-        DRAW: 'draw'
+        DRAW: 'draw',
+        TEXT: 'text'
     }
 };
 
@@ -34,30 +37,39 @@ $(function () {
 
     
     
-    
     $('#my-canvas').on('mousedown', function(mouseEvent) {
         switch (drawio.selectedShape) {
             case drawio.availableShapes.RECTANGLE:
             drawio.selectedElement = new Rectangle({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, 0, 0, drawio.colorPick);
             break;
             case drawio.availableShapes.LINE:
-            drawio.selectedElement = new Line({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, drawio.colorPick, 0);
+            drawio.selectedElement = new Line({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, drawio.colorPick, drawio.widthPick);
             break;
             case drawio.availableShapes.CIRCLE:
-            drawio.selectedElement = new Circle({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, drawio.colorPick, 0);
+            drawio.selectedElement = new Circle({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, drawio.colorPick, drawio.widthPick);
             break;
             case drawio.availableShapes.DRAW:
-            drawio.selectedElement = new Draw({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, drawio.colorPick, 0);
+            drawio.selectedElement = new Draw({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, drawio.colorPick, drawio.widthPick);
             break;
-    
+            case drawio.availableShapes.TEXT:
+            drawio.selectedElement = new Text({ x: mouseEvent.offsetX, y: mouseEvent.offsetY }, drawio.textBox, 0, 0);
+            $(drawio.textBox).css({"top": mouseEvent.pageY, "left": mouseEvent.pageX});
+			$(drawio.textBox).show();
+            break;
         }
     });
     $('#my-canvas').on('mousemove', function(mouseEvent) {
        if (drawio.selectedElement) {
+           if(drawio.selectedShape == "text") {
+               $(drawio.textBox).css({"top": mouseEvent.pageY, "left": mouseEvent.pageX});
+               drawio.selectedElement.resize(mouseEvent.offsetX, mouseEvent.offsetY);
+           }
+           else {
             console.log(drawio.selectedElement);
             drawio.ctx.clearRect(0, 0, drawio.canvas.width, drawio.canvas.height);
             drawio.selectedElement.resize(mouseEvent.offsetX, mouseEvent.offsetY);
             drawCanvas();
+           }
         }
     });
 
@@ -65,5 +77,25 @@ $(function () {
         drawio.shapes.push(drawio.selectedElement);
         console.log(drawio.shapes);
         drawio.selectedElement = null;
-    })
+    });
+    $('#idTextBox').on('keyup', function(event){
+        event.preventDefault();
+        console.log('hi');
+        if (event.keyCode === 13) {
+            var theText = $(this).val();
+            drawio.selectedElement.render(theText);
+            drawio.shapes.push(theText);
+            $("#idTextBox").val('');
+	        $("#idTextBox").hide();
+        }
+    });
 });
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
--- shapes.js
+++ shapes.js
@@ -14,18 +14,30 @@ function Rectangle(position, width, height, colorPick) {
     this.height = height;
     this.colorPick = colorPick.value;
 };
-function Line(position, colorPick) {
+function Line(position, colorPick, widthPick) {
     Shape.call(this, position);
     this.colorPick = colorPick.value;
+    this.widthPick = widthPick.value;
 };
-function Circle(position, colorPick) {
+function Circle(position, colorPick, widthPick) {
     Shape.call(this, position);
     this.colorPick = colorPick.value;
+    this.widthPick = widthPick.value;
 };
-function Draw(position, colorPick) {
+function Draw(position, colorPick, widthPick) {
     Shape.call(this, position);
     this.colorPick = colorPick.value;
+    this.widthPick = widthPick.value;
     this.arrx = [];
+};
+function Text(position, textBox, width, height) {
+    Shape.call(this, position);
+    this.width = width;
+    this.height = height;
+    this.textBox = textBox.value;
+
+    console.log(this.position.x, this.position.y)
+    
 };
 
 
@@ -33,21 +45,23 @@ Rectangle.prototype = Object.create(Shape.prototype);
 Line.prototype = Object.create(Shape.prototype);
 Circle.prototype = Object.create(Shape.prototype);
 Draw.prototype = Object.create(Shape.prototype);
+Text.prototype = Object.create(Shape.prototype);
 
 
 Rectangle.prototype.constuctor = Rectangle;
 Line.prototype.constuctor = Line;
 Circle.prototype.constuctor = Circle;
 Draw.prototype.constuctor = Draw;
+Text.prototype.constuctor = Text;
 
 Rectangle.prototype.render = function() {
-    drawio.ctx.fillStyle = drawio.colorPick.value;
+    drawio.ctx.fillStyle = this.colorPick;
     drawio.ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
 };
 Line.prototype.render = function() {
     drawio.ctx.beginPath();
     drawio.ctx.strokeStyle = this.colorPick;
-    drawio.ctx.lineWidth = "10";
+    drawio.ctx.lineWidth = this.widthPick;
     drawio.ctx.moveTo(this.position.x, this.position.y);
     drawio.ctx.lineTo(this.position.x + this.width, this.position.y + this.height);
     drawio.ctx.stroke();
@@ -55,7 +69,7 @@ Line.prototype.render = function() {
 Circle.prototype.render = function() {
     drawio.ctx.beginPath();
     drawio.ctx.strokeStyle = this.colorPick;
-    drawio.ctx.lineWidth = "10";
+    drawio.ctx.lineWidth = this.widthPick;
     drawio.ctx.arc(this.position.x, this.position.y, Math.abs(this.width) , 0, 2 * Math.PI);
     drawio.ctx.stroke();
 };
@@ -63,7 +77,7 @@ Draw.prototype.render = function() {
     
     drawio.ctx.beginPath();
     drawio.ctx.strokeStyle = this.colorPick;
-    drawio.ctx.lineWidth = "5";
+    drawio.ctx.lineWidth = this.widthPick;
     drawio.ctx.moveTo(this.position.x, this.position.y);
     for(var i = 1; i<this.arrx.length; i++) {
         var x = this.arrx[i].x;
@@ -73,7 +87,10 @@ Draw.prototype.render = function() {
     drawio.ctx.stroke();
     drawio.ctx.closePath();
 };
-
+Text.prototype.render = function(theText) {
+    drawio.ctx.font = "30px Arial";
+    drawio.ctx.fillText(theText, this.position.x, this.position.y);
+};
 
 Rectangle.prototype.resize = function (x, y) {
     this.width = x - this.position.x;
@@ -91,6 +108,10 @@ Draw.prototype.resize = function (x, y) {
     this.width = x - this.position.x;
     this.height = y - this.position.y;
     this.arrx.push({x: x , y: y});
+};
+Text.prototype.resize = function (x, y) {
+    this.width = x - this.position.x;
+    this.height = y - this.position.y;
 };
 
 
